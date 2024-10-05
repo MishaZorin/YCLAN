540914fc80eb57f5251bf8cf62e9426cb5c74d4b
MishaZorin <165201586+MishaZorin@users.noreply.github.com>
2024-10-05 21:46:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let about_clan = document.getElementById('about_clan')
+
+let about_clan_modal = document.getElementById('about_clan_modal')
+
+about_clan.onclick=function(){
+    about_clan_modal.style.display = 'block'
+    
+
+}
